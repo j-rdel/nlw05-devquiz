8207ac02c635d3307ef1b3613f001dad292e8890
Jardel Urban <jardelurban3@gmail.com>
2021-11-02 03:43:42 -0300

    splash_page finalizada e inicio da appBar da home_page finalizado

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,12 +1,10 @@
-// import 'package:devquiz/home/home_page.dart';
-// import 'package:devquiz/splash/splash_page.dart';
+import 'package:DevQuiz/home/home_page.dart';
+import 'package:DevQuiz/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "DevQuiz",
-    );
+    return MaterialApp(title: "DevQuiz", home: HomePage());
   }
 }
---
+++ lib/splash/splash_page.dart
@@ -0,0 +1,16 @@
+import 'package:DevQuiz/core/app_gradients.dart';
+import 'package:DevQuiz/core/app_images.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          decoration: BoxDecoration(gradient: AppGradients.linear),
+          child: Center(child: Image.asset(AppImages.logo))),
+    );
+  }
+}
