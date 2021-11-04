e24bc7e8335a8f521f302593442a7a6c5e026b7b
Jardel Urban <jardelurban3@gmail.com>
2021-11-04 03:18:14 -0300

    Criando widget quiz e answer

--- lib/chalange/challange_page.dart
+++ lib/chalange/challange_page.dart
@@ -1,4 +1,5 @@
 import 'package:DevQuiz/chalange/widgets/question_indicator/question_indicator_widget.dart';
+import 'package:DevQuiz/chalange/widgets/quiz/quis_widget.dart';
 import 'package:flutter/material.dart';
 
 class ChallangePage extends StatefulWidget {
@@ -12,9 +13,9 @@ class _ChallangePageState extends State<ChallangePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-          preferredSize: Size.fromHeight(60),
-          child: SafeArea(child: QuestionIndicatorWidget())),
-    );
+        appBar: PreferredSize(
+            preferredSize: Size.fromHeight(60),
+            child: SafeArea(child: QuestionIndicatorWidget())),
+        body: QuizWidget(title: "O que o Flutter faz em sua totalidade?"));
   }
 }
